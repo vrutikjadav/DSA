ebce93a5f7a120113dfdc5a5e9bed8c85c34d5f5
Vrutik Jadav <52519269+vrutikjadav@users.noreply.github.com>
2025-07-28 18:54:53 +0000

    bubblesort logic bulding done compare each element by its previous element if it is smaller than the previous element then swap each other do rest to the all elements

---
+++ DSA/BubbleSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;    
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Unsorted array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << ", ";
+    }
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            if(arr[j-1] > arr[j])
+            {
+                swap(arr[j-1],arr[j]);
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << ", ";
+    }
+    return 0;
+}
